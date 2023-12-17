c9ca2104867cf3c3e6699907421742dbe7d4329f
ThibautDev <thibaut.props@icloud.com>
2023-12-17 20:34:31 +0100

    fix code-runner.lua config

--- lua/thibautdev/plug/code-runner.lua
+++ lua/thibautdev/plug/code-runner.lua
@@ -1,6 +1,22 @@
 return { 
     "CRAG666/code_runner.nvim",
     config = function()
+        require('code_runner').setup({
+            filetype = {
+                python = "python3 -u",
+                typescript = "deno run",
+                rust = {
+                    "cd $dir &&",
+                    "rustc $fileName &&",
+                    "$dir/$fileNameWithoutExt"
+                },
+                c = {
+                    "cd $dir &&",
+                    "gcc $fileName -o $fileNameWithoutExt &&",
+                    "$dir/$fileNameWithoutExt"
+                }
+            },
+        })
         -- set keymaps
         local keymap = vim.keymap -- for conciseness
 
